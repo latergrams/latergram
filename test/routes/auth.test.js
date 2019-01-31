3fb98c87943d7a5835a2fee7e8c9b3cfe5a92364
Carmen Perezchica <crperezchica@gmail.com>
2019-01-30 16:07:49 -0800

    created server.js. app.js and started auth test

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,3 +1,27 @@
 const request = require('supertest');
 const app = require('../lib/app');
 const mongoose = require('mongoose');
+const connect = require('../../lib/utils/connect');
+
+describe('app', () => {
+  beforeAll(() => {
+    connect();
+  });
+  beforeEach(done => {
+    mongoose.connection.dropDatabase(done);
+  });
+  afterAll(done => {
+    mongoose.connection.close(done);
+  });
+
+  it('can signup a user', () => {
+    return request(app)
+      .post('/auth/signup')
+      .send({
+        username: 'smith'
+        password: 'password'
+        photoUrl: 'www.amazon.com/mygratestphoto'
+      })
+  });
+
+});
